c0e3550fd7ad2c5ed5f76473e1c744dfae145535
Sergey Rudenko <sergeyrudenkosr@gmail.com>
2025-08-25 16:15:28 +0300

    Added the AXIOS library API. I wrote an asynchronous function for the weather request.  Added keys to a variable environment for safety

--- bot.js
+++ bot.js
@@ -1,7 +1,19 @@
-const { Telegraf } = require('telegraf')
+const {Telegraf} = require('telegraf')
+const axios = require('axios');
+require('dotenv').config();
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
 bot.start((ctx) => ctx.reply('Welcome'))
+bot.on('message', async (ctx) => {
+    // if (ctx.message.location) {
+    console.log(ctx.message.location);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&units=metric&appid==${process.env.WEATHER_API}`
+    const response = await axios.get(url);
+    console.log(response);
+    ctx.reply(`${response.data.name}: ${response.data.main.temp} °C`);
+})
 bot.launch()
 
 // Enable graceful stop
